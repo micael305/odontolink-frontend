bb08c1b3c8a72ddabdaab17919951b227df49696
micael305 <miicael305@gmail.com>
2025-10-29 08:27:59 -0300

    Estructura inicial

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import './components/Login.jsx';
+import './components/Home.jsx';
+
+function App() {
+  const [Usuario, setUsuario] = useState(null);
+
+  return (
+    <>
+   
+    </>
+  )
+}
+
+export default App
