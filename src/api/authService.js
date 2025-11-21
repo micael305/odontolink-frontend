08724cb724f23d41070b216b0ce9da13c67bc4af
micael305 <miicael305@gmail.com>
2025-11-21 20:53:52 -0300

    fix: timeout actualizado

--- src/api/authService.js
+++ src/api/authService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const publicApi = axios.create({
   baseURL: `${import.meta.env.VITE_API_BASE_URL}/auth`,
-  timeout: 5000,
+  timeout: 10000,
 });
 
 export const login = async (email, password) => {
